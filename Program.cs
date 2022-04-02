a7745852d79a47ac1f1639707c2ab1502f98fdd9
Maria Eduarda Bedetti Amorim <Conta.para.qualquer.cois2005@gmail.com>
2022-04-01 21:38:37 -0300

    Finaliza o Exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome, SOBRENOME;
+
+Console.Write("Digite seu primeiro nome: ");
+nome=Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome=Console.ReadLine()!;
+
+SOBRENOME=sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}"!);
+Console.WriteLine($"Nome de catalogo: {SOBRENOME} {nome} "!);
